d7e834524e077e629b53d14b2d505d8df4671591
awantech-io <hello.wanlab@gmail.com>
2020-10-25 01:08:25 +0800

    auth middleware

--- routes/auth.js
+++ routes/auth.js
@@ -3,15 +3,23 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const auth = require('../middleware/auth');
 const { check, validationResult } = require('express-validator'); 
 const User = require('../models/User');
+
 
 // @route GET api/auth
 // @desc  Get logged in user
 // @access Private
 
-router.get('/', (req, res) => {
-  res.send('Log in user');
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Sever Error');
+  }
 });
 
 
@@ -41,7 +49,7 @@ router.post(
         return res.status(400).json({ msg: 'Invlaid Credentials' });
       }
 
-      // check password
+      ///check password
       const isMatch = await bcrypt.compare(password, user.password);
 
       if (!isMatch) {
