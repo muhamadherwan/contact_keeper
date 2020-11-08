d298ea2e34996c5bc45703307e97b21fd8606300
awantech-io <hello.wanlab@gmail.com>
2020-11-08 13:08:57 +0800

    put contact route

--- routes/contacts.js
+++ routes/contacts.js
@@ -64,8 +64,40 @@ async (req, res) => {
 // @route PUT api/contacts/:id
 // @desc  Update contacts
 // @access Private
-router.put('/:id', (req, res) => {
-  res.send('Update contact');
+router.put('/:id', auth, async (req, res) => {
+  const { name, email, phone, type } = req.body;
+
+  // build contact object
+  const contactFields = {};
+  if(name) contactFields.name = name;
+  if(email) contactFields.email = email;
+  if(phone) contactFields.phone = phone;
+  if(type) contactFields.type = type;
+
+  try {
+    let contact = await Contact.findById(req.params.id);
+    
+    if(!contact) return res.status(404).json({ msg: 'Contact not found'});
+
+    // make sure user own contact
+    if(contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Not authorized' });
+    }
+
+    contact = await Contact.findByIdAndUpdate(
+      req.params.id, 
+      { $set: contactFields },
+      { new: true }
+    );
+
+    res.json(contact);
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('server error');
+  }
+
+
 });
 
 // @route DELETE api/contacts/:id
