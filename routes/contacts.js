a944748bd86cb1604f15cb62fd5e200e1d17b6d8
awantech-io <hello.wanlab@gmail.com>
2020-11-08 13:25:12 +0800

    delete contact routes

--- routes/contacts.js
+++ routes/contacts.js
@@ -97,14 +97,31 @@ router.put('/:id', auth, async (req, res) => {
     res.status(500).send('server error');
   }
 
-
 });
 
 // @route DELETE api/contacts/:id
 // @desc  Delete contacts
 // @access Private
-router.delete('/:id', (req, res) => {
-  res.send('Delete contact');
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    let contact = await Contact.findById(req.params.id);
+    
+    if(!contact) return res.status(404).json({ msg: 'Contact not found'});
+
+    // make sure user own contact
+    if(contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Not authorized' });
+    }
+
+    await Contact.findByIdAndRemove(req.params.id);
+
+    res.json({ msg:'Contact removed'});
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('server error');
+  }
+
 });
 
 module.exports = router;
