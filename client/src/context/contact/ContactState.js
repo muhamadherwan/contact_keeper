ec9a1185424acdc9151175cef41f8bdcb65857c4
awantech-io <hello.wanlab@gmail.com>
2020-11-15 01:33:20 +0800

    add contact data

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react';
-import uuid from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer'; 
 
@@ -43,7 +43,12 @@ const ContactState = props => {
   const [state, dispatch] = useReducer(contactReducer, initialState);
   
   // Add Contact
+  const addContact = contact => {
+    const id = uuidv4();
 
+    
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  }
   // Delete Contact
 
   // Set Current Contact
@@ -57,7 +62,8 @@ const ContactState = props => {
   return (
     <ContactContext.Provider
       value={{
-        contacts: state.contacts
+        contacts: state.contacts,
+        addContact
       }}>
       {props.children}
     </ContactContext.Provider>
