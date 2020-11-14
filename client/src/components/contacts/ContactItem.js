9968490ea572c3585d565fbe4d1c4c063cc14995
awantech-io <hello.wanlab@gmail.com>
2020-11-15 00:13:19 +0800

    upd contact component

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
 const ContactItem = ( {contact} ) => {
   const { id, name, email, phone, type } = contact;
@@ -7,7 +8,9 @@ const ContactItem = ( {contact} ) => {
     <div className='card bg-light'>
       <h3 className='text-primary text-left'>
         {name}{' '}
-        <span className={
+        <span 
+          style={{ float: 'right' }}
+          className={
           'badge '+
           (type === 'professional' ? 'badge-success' : 'badge-primary')
         }
@@ -15,9 +18,26 @@ const ContactItem = ( {contact} ) => {
           {type.charAt(0).toUpperCase() + type.slice(1)}
 
         </span>
-      </h3>  
+      </h3>
+
+      <ul className="list">
+        {email && (<li>
+          <i className="fas fa-envelope-open"></i> {email}
+        </li>)}
+        {phone && (<li>
+          <i className="fas fa-phone"></i> {phone}
+        </li>)}
+      </ul>
+      <p>
+        <button className="btn btn-dark btn-sm">Edit</button>
+        <button className="btn btn-danger btn-sm">Delete</button>
+      </p>  
     </div>
   )
+}
+
+ContactItem.propTypes = {
+  contact: PropTypes.object.isRequired
 }
 
 export default ContactItem
