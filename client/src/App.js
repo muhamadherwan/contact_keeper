05a538e25809c1022540f1efe841d79d027b1821
awantech-io <hello.wanlab@gmail.com>
2020-11-18 23:41:29 +0800

    alert component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
+import Alerts from './components/layout/Alerts';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
+
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
+import AlertState from './context/alert/AlertState';
 import './App.css';
 
 
@@ -15,10 +18,12 @@ const App = () => {
   return (
     <AuthState>
     <ContactState>
+    <AlertState>
     <Router>
     <Fragment>
       <Navbar />
       <div className="container">
+        <Alerts />
         <Switch>
           <Route exact path='/' component={Home} />
           <Route exact path='/about' component={About} />
@@ -28,6 +33,7 @@ const App = () => {
       </div>
       </Fragment>
     </Router>
+    </AlertState>
     </ContactState>
     </AuthState>
   );
